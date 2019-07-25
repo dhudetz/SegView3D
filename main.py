ccba76fe904bfa0999b8a5bfddb0a6fd94f3deeb
Dan <dannyhudetz@gmail.com>
2019-07-25 16:04:46 -0500

    added chunk multiprocessing

--- main.py
+++ main.py
@@ -10,32 +10,14 @@ import pygame.locals as lcl
 import OpenGL.GL as GL
 import OpenGL.GLU as GLU
 
-verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-    )
+import h5py as hdf
 
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+import multiprocessing
+import threading
 
+fileLocation="\\\\wales.es.anl.gov\\DataArchive\\Software\\SegView\\sample_data\dataset_01.hdf5"
+points=[]
+dataSet=None
 
 move_map = {pygame.K_w: ( 0, 0, .1),
             pygame.K_s: ( 0, 0,-.1),
@@ -58,16 +40,17 @@ def checkKeys():
         else:
             GL.glTranslatef(direction[0], direction[1], direction[2])
     
-
-def Cube():
-    GL.glBegin(GL.GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            GL.glVertex3fv(verticies[vertex])
+#
+#def calculatePoints():
+#    for 
+    
+def points():
+    GL.glBegin(GL.GL_POINTS)
+    GL.glVertex3fv()
     GL.glEnd()
 
 def main():
-    global count
+    global count, dataSet
     pygame.init()
     display = (1000,750)
     pygame.display.set_mode(display, lcl.DOUBLEBUF|lcl.OPENGL)
@@ -75,8 +58,11 @@ def main():
     GLU.gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
     
     GL.glTranslatef(0.0,0.0, -5)
-    GL.glPointSize(100)
-
+    GL.glPointSize(10)
+    file=hdf.File(fileLocation, 'r')
+    dataSet=file.get(list(file.items())[0][0])
+    chunkify(4)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -86,8 +72,18 @@ def main():
         checkKeys()
 #        GL.glRotatef(1, 3, 1, 1)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT|GL.GL_DEPTH_BUFFER_BIT)
-        Cube()
+        points()
         pygame.display.flip()
         pygame.time.wait(10)
+
+def chunkify(numChunks):
+    dataLength=len(dataSet)
+    chunkSize=int(dataLength/numChunks)
+    chunks=[]
+    chunks.append((0,chunkSize-1))
+    for i in range(2, numChunks-1):
+        chunks.append((chunkSize*(i-1)), chunkSize*i)
+    chunks.append((chunkSize*(numChunks-1), dataLength))
+    print(chunks)
 
 main()
